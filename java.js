7b68a4eb16522610611bd0c411edd6f82dfebf4f
Monty Wood (1000064524) <WOODMG3@student.op.ac.nz>
2020-11-19 11:18:14 +1300

    coments added

--- java.js
+++ java.js
@@ -1,33 +1,33 @@
 /*text fetch from link*/
 
 fetch("https://baconipsum.com/api/?type=all-meat&paras=2&start-with-lorem=1")
-.then(
-    res=>{
-        return res.json();
-    }
-).then(data=>{
-    let para = document.getElementById("ranPara");
-    para.innerHTML=data;
+    .then(
+        res => {
+            return res.json();
+        }
+    ).then(data => {
+        let para = document.getElementById("ranPara");
+        para.innerHTML = data;
 
-});
+    });
 
 /*function to make login*/
 
-$(document).ready(function() { 
+$(document).ready(function () {
 
 
-    $('#Loginbu').click(function() {
+    $('#Loginbu').click(function () {
         event.preventDefault();
         let Validusername = $('#logname').val() === 'user';
         let ValidPassword = $('#logpass').val() === 'P@ssw0rd123';
-    
-     
-    
+
+
+
         if (Validusername === true && ValidPassword === true) {
             window.location = "index.html";
         }
         else {
-        alert("wrong.")
+            alert("wrong.")
         }
     });
 });
